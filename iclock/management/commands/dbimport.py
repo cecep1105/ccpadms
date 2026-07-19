ef7a3afa22fd296ffa7721367b8fdc0b5d7b2bc1
cecep1105 <cecep1105@gmail.com>
2026-07-19 21:07:55 +0700

    modified:   iclock/management/commands/dbimport.py

--- iclock/management/commands/dbimport.py
+++ iclock/management/commands/dbimport.py
@@ -69,6 +69,7 @@ class Command(BaseCommand):
         if cmd[0] == "POOLS":
             from sqlalchemy import MetaData,Table,select,func,desc,create_engine
             from datetime import datetime, timedelta
+            from iclock.models import department
             import ipaddress
 
             ten_days_ago = datetime.now() - timedelta(days=10)
@@ -91,65 +92,25 @@ class Command(BaseCommand):
                 ip_octets[3] = '0/24'
                 network = '.'.join(ip_octets)
                 netid = ip_octets[2]
+                pool = r._mapping[iclock.c.POOL]
 
-                print(f"{i},{r._mapping[iclock.c.POOL]},{netid},{router},{network}")
+                try:
+                    dept = department.objects.get(DeptName=pool)
+                except Exception as e:
+                    print(e)
+                    dept = department(
+                        DeptID = i,
+                        DeptName = pool,
+                        NetID = netid,
+                        DeptRouter = router,
+                        DeptSubnet = network
+                    )
+                    dept.save()
+
+                    print(f"{i},{pool},{netid},{router},{network}")
 
 
-        if cmd[0] == "coba":
-            import sqlalchemy
-            engine = sqlalchemy.create_engine('mysql+pymysql://adms:ad123@172.16.10.35/dbabsen')
-            conn = engine.connect()
-            metadata = sqlalchemy.MetaData()
-            iclock = sqlalchemy.Table('absensi_iclock', metadata,autoload_with=engine)
-            query = sqlalchemy.select(iclock).filter(iclock.columns.POOL.notin_(['MOBILE','TEST','TESTING']),sqlalchemy.func.char_length(iclock.SN)>10)
-            result = conn.execute(query)
-            resulset = result.fetchall()
-            for r in resulset:
-                # 'OID6120056120100264', 'KLDHRC1', 'KARYAWAN', 'KLENDER', 1, datetime.datetime(2024, 12, 3, 13, 41, 58,
-                #                                                                               711625), '00:00;14:05', 1, '9999', '9999', None, '192.168.25.94
-                row = "%s;%s;%s;%s" % (r[0], r[1], r[2], r[3])
 
-
-        if cmd[0] == "coba2":
-            from apps.iclock.models import department
-            dname = params[0].split(';')[0]
-            drouter = params[0].split(';')[1]
-            dsubnet = params[0].split(';')[2]
-            dparent = params[0].split(';')[3]
-            dnetid = params[0].split(';')[4]
-            dept = department(
-                DeptName=dname,
-                DeptRouter=drouter,
-                DeptSubnet=dsubnet,
-                parent=dparent,
-                NetID=dnetid
-            )
-            dept.save()
-        if cmd[0] == "coba3":
-            with connections['old'].cursor() as cursor:
-                query = """ select * from iclock_employee """
-                cursor.execute(query)
-                row = cursor.fetchall()
-                result = row
-                print(result)
-
-        if cmd[0] == "copydata":
-            from ftplib import FTP
-            import pandas as pd
-            ftp = FTP('172.16.10.35')
-            ftp.login('absen','ab123')
-
-            d2='01/01/2025'
-            d1='01/01/2025'
-            print(pd.date_range('01/01/2025','01/05/2025'))
-            ftpfiles = ["%s/masterlog/%s.txt" % ('djangoapp/mysite', d.strftime('%m%Y/%d')) for d in
-                        pd.date_range(d2, d1)]
-            for ftpf in ftpfiles:
-                print(ftpf)
-                r = []
-                ftp.retrlines('RETR %s' % ftpf,r.append)
-                for line in r:
-                    print(line)
 
 
 
