29e61aa25f7c8f006c62c0a61662291bd310baeb
cecep1105 <cecep1105@gmail.com>
2026-08-04 20:31:03 +0700

    modified:   config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -646,3 +646,6 @@ ITINFRA_ENCRYPTION_KEY = env('ITINFRA_ENCRYPTION_KEY', default='')
 MIRROR_REQUEST = env.bool('MIRROR_REQUEST', default=False)  # kalau True, semua request pushsdk akan dikirim ke host mirror
 MIRROR_REQUEST_URL = env('MIRROR_REQUEST_URL', default='')  # mis. 'http://mirror.example.com:8000', kalau kosong, mirror request TIDAK dikirim sama sekali (meski MIRROR_REQUEST=True)
 SEND_ORIGINIP=env('ORIGINIP', default=False) # kalau True, IP asli pengirim request akan dikirim ke mirror via header X-Forwarded-For (untuk keperluan logging/traceability di server mirror)
+
+#--- Legacy iClock (DB lama) -- untuk keperluan migrasi data dari DB lama ke DB baru
+ICLOCK_LEGACY_DBCRED=env('ICLOCK_LEGACY_DBCRED', default='')  
